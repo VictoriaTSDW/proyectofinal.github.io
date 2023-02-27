eaa93572f1165c6e8b174d7d385977c120dc6fd1
victoriacuevasbarea <victoriacuevasbarea@gmail.com>
2023-02-27 18:38:25 -0300

    agregando archivos

---
+++ js.js
@@ -0,0 +1,47 @@
+var botonSubmit = document.getElementById("boton-submit")
+botonSubmit.addEventListener("click", clicked)
+
+function clicked(){
+    alert("Mensaje Enviado");
+    document.getElementById("boton-submit").innerHTML = "Mensaje Enviado"
+}
+
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("photoshop");
+        habilidades[3].classList.add("comunicacion");
+        habilidades[4].classList.add("trabajo");
+        habilidades[5].classList.add("creatividad");
+        habilidades[6].classList.add("dedicacion");
+        habilidades[7].classList.add("proyect");
+    }
+}
+
+window.onscroll = function(){
+    efectoHabilidades();
+} 
+
+
+
+let menuVisible = false;
+//Función que oculta o muestra el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    //oculto el menu una vez que selecciono una opcion
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
